57e44f3fd848f8217e12e72936652bcadb2a55e7
lkportal <lksxim@gmail.com>
2022-12-22 21:30:48 -0300

    sec

--- scripts/main.js
+++ scripts/main.js
@@ -1,23 +1,62 @@
 const ListDg_html = document.getElementById('lisdg')
+let quantidade = 10 
+function createelements({name,img,level}){
 
-function  ListDigimon(props){
-    return(`
-    
-    <div class="cont-main">
-            <img src=${props.img} alt="">
-            <ul class="list">
-                <li>${props.name}</li>
-                <li>${props.level}</li>
-            </ul>
-        </div>
-    
-    `)
+    const div = document.createElement('div')
+    div.classList.add('cont-main')
+    const div2 = document.createElement('div')
+    div2.classList.add('cont-item')
+    const div3 = document.createElement('div')
+    div3.classList.add('cont-head')
+    const imgs = document.createElement('img')
+    imgs.classList.add('img')
+    imgs.setAttribute('src',img)
+    const ul = document.createElement('ul')
+    ul.classList.add('list')
+    const li =document.createElement('li')
+    li.classList.add('li')
+    li.innerHTML = `<strong>Nome</strong>:${name}`
+    const li2 =document.createElement('li')
+    li2.classList.add('li')
+    li2.innerHTML = `<strong>Nivel</strong>:${level}`
+    div.appendChild(div2)
+    div.appendChild(div3)
+    div2.appendChild(imgs)
+    div3.appendChild(ul)
+    ul.appendChild(li)
+    ul.appendChild(li2)
+    document.body.appendChild(div)
+
 }
+
+
+const btns = document.getElementById('btn')
+ 
+ async function coletaDados () {
+     const outPut = document.getElementById('output').value
+     console.log(outPut)
+   return quantidade = outPut
+}
+
+btns.addEventListener('click',coletaDados)
+ 
+
+
+
+
+  
+function  ListDigimon(props){
+    
+    return createelements(props)
+}
+
+
+
 Apifecth
     .then(list => {
-        const limite = list.slice(0,10)
+        const limite = list.slice(0,quantidade)
         limite.map(e =>{
-            return ListDg_html.innerHTML += ListDigimon(e)
+          return ListDg_html.innerHTML += ListDigimon(e);
         })
     })
 
